20eac0c9ca16bac5608f3e953d57f9bc85100a5f
EricTrainingRev <eric.suminski@revature.com>
2024-12-18 10:39:50 -0500

    adding some comments to test runner

--- Week 2/WikiAutomation/src/test/java/com/revature/TestRunner.java
+++ Week 2/WikiAutomation/src/test/java/com/revature/TestRunner.java
@@ -9,14 +9,33 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/*
+    The RunWith annotation tell Junit to use the Cucumber testing framework for running our tests. We can still
+    do our setup and teardown via Junit, but Junit does not have a native way of interacting with feature files.
+    Any steps and hooks we design with Cucumber in mind can have their testing facilitated by Junit using Cucumber
+    as the testing engine
+*/
 @RunWith(Cucumber.class)
+/*
+    We also need to tell Cucumber where to find the features and glue code for the tests. The CucumberOptions
+    annotation allows us to provide this information to Cucumber programatically. Features is where you provide
+    the location/s of your feature files, glue is where you provide the location/s of your step definitions.
+
+    Include also is the plugin option: you can provide instructions for using non-standard Cucumber features
+    with this configuration. The pretty plugin makes the test results easier to read and understand in the
+    terminal, and the html plugin creates an html report of the test results
+*/
 @CucumberOptions(
         features = "classpath:features",
         glue = "com.revature.steps",
-        plugin = "pretty"
+        plugin = {"pretty","html:src/test/resources/reports/html-report.html"}
 )
 public class TestRunner {
 
+    /*
+        Since these fields are going to be used throughout the various steps we implement we make them
+        public and static for ease of access
+    */
     public static WebDriver driver = null;
     public static WikiHome wikiHome;
 
